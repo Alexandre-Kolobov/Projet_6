f1f90ed0f60f5ccd80d6ab21c7a885c85a67a6d1
Alexandre Kolobov <kolobovalexandre@gmail.com>
2023-05-29 21:40:50 +0200

    delet old files

--- fetch.js
+++
@@ -1,5 +0,0 @@
-// // fetch API meilleur film:
-// const reponse = await fetch('http://localhost:8000/api/v1/titles/');
-// console.log(reponse);
-// let films = await reponse.json();
-// console.log(films);
